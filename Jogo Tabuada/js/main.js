6385d19bbca5ba7b7d53560be17a3470889139b1
GHilario <39409550+GHilario@users.noreply.github.com>
2018-08-13 17:41:12 -0300

    Add files via upload

---
+++ Jogo Tabuada/js/main.js
@@ -0,0 +1,51 @@
+var n1,n2,resultado;
+var nivel=10;
+
+function sorteando(){
+	n1=Math.floor(Math.random() * nivel); 
+	n2=Math.floor(Math.random() * nivel); 
+	resultado=n1*n2;
+	//imprimindo na tela os numeros sorteados
+	var texto= "<p>"+n1+" * "+n2+"= </p>";
+	$("#sort").html(texto);
+	$("#sort").addClass("tamfonte");
+}
+
+//conferindo resultado
+$("#conferir").click(function(){
+
+
+	var resp=$("#resposta").val();
+
+	if(resp==resultado)
+		alert("Resposta Correta");
+	else
+		alert("Resposta Errada\nA resposta correta seria "+resultado);
+
+	//sorteando um novo numero
+	$("#resposta").val(" ")
+	sorteando();
+
+});
+
+//escolhendo dificuldades
+
+$("#facil").click(function(){
+	nivel=10;
+	sorteando();
+});
+
+$("#medio").click(function(){
+	nivel=100;
+	sorteando();
+});
+
+$("#dificil").click(function(){
+	nivel=1000;
+	sorteando();
+});
+
+
+//Iniciando 
+sorteando();
+
